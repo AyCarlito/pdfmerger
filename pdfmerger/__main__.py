1b20eff32a079d3e7d4b506c03cc703d7f3a8bc0
Daniel Hislop <DanielPC>
2021-04-15 22:50:29 +0100

    Fix Recursion Problem

--- pdfmerger/__main__.py
+++ pdfmerger/__main__.py
@@ -1,11 +1,13 @@
 from PyPDF2 import PdfFileMerger
 import os
 import glob
+import sys
+sys.setrecursionlimit(1500)
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-for dir in glob.glob(os.getcwd() + "/*/"):
-    os.chdir(dir)
+for directory in glob.glob(os.getcwd() + "/*/"):
+    os.chdir(directory)
     dirname = os.getcwd().split("\\")[-1]
     merger = PdfFileMerger()
     try:
